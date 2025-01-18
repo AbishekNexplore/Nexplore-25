9ee1a44dc2a98107642773ff55a882ecd1a3c296
ABISHEK S <abishek642005@gmail.com>
2025-01-19 00:13:03 +0530

    main navbar orient

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import { Box, Container, useTheme } from '@mui/material';
 import Navbar from './Navbar';
-import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
     const theme = useTheme();
 
     return (
-        <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-            <Sidebar />
-            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
-                <Navbar />
-                <Box
-                    component="main"
-                    sx={{
-                        flexGrow: 1,
-                        bgcolor: theme.palette.background.default,
-                        p: 3,
-                        mt: 8
-                    }}
-                >
-                    <Container maxWidth="lg">
-                        {children}
-                    </Container>
-                </Box>
+        <Box sx={{ 
+            display: 'flex', 
+            flexDirection: 'column', 
+            minHeight: '100vh',
+            '& .MuiAppBar-root': {
+                '& .MuiToolbar-root': {
+                    backgroundColor: theme.palette.primary.main,
+                    minHeight: '64px'
+                }
+            }
+        }}>
+            <Navbar />
+            <Box
+                component="main"
+                sx={{
+                    flexGrow: 1,
+                    mt: '64px', // Fixed height for navbar
+                    pt: 3,
+                    pb: 3,
+                    backgroundColor: theme.palette.background.default,
+                    position: 'relative',
+                    zIndex: 1
+                }}
+            >
+                <Container maxWidth="lg">
+                    {children}
+                </Container>
             </Box>
         </Box>
     );
